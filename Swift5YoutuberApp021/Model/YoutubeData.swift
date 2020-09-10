b9ad7f54132d899537cc651d2ceadf858fb7b4b0
Fujio Nakatsuka <com.nifty.cbm01799>
2020-09-10 14:40:44 +0900

    イベント用に使用していたGmailアドレスでFirebaseにアプリを登録してからGoogle Youtube Data APIで新規にキーを作成したが、結果はビルド中にクラッシュ domain = global;                 message = "Requests from this ios client application <empty> are blocked.";                 reason = forbidden;             }

---
+++ Swift5YoutuberApp021/Model/YoutubeData.swift
@@ -0,0 +1,21 @@
+//
+//  YoutubeData.swift
+//  Swift5YoutuberApp021
+//
+//  Created by 中塚富士雄 on 2020/09/10.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import Foundation
+class YoutubeData{
+    
+    //取得したいものの値を保持する
+    
+    var videoId:String = ""
+    //var pubulishedAt:String = ""
+    var title:String = ""
+    var imageURLString:String = ""
+    var youtubeURL:String = ""
+    var channelTitle:String = ""
+}
+
